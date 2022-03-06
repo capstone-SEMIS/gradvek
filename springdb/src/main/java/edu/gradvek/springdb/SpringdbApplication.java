2a0e47b138c36c4e2cceb4a21a7a80182e045214
Yuri Machkasov <ymachkasov@interactions.com>
2022-03-06 10:54:14 -0500

    yuri: adding spring template

---
+++ springdb/src/main/java/edu/gradvek/springdb/SpringdbApplication.java
@@ -0,0 +1,13 @@
+package edu.gradvek.springdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdbApplication.class, args);
+	}
+
+}
---
+++ springdb/src/test/java/edu/gradvek/springdb/SpringdbApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.gradvek.springdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
