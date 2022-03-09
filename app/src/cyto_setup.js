d49dd9556021abe22f234a19c4edf7cb9cc2563a
Aleksander Aleksiev <aleksaleksiev@outlook.com>
2022-03-10 00:09:02 +1100

    Insert additional dummy data and pathway vis

--- app/src/cyto_setup.js
+++ app/src/cyto_setup.js
@@ -5,27 +5,58 @@ import popper from 'cytoscape-popper';
 let initialElements = [
     {
         group: "nodes",
+        classes: ["pathway"],
+        data: {
+            id: "pathway_xyz"
+        }
+    },
+    {
+        group: "nodes",
+        classes: ["drug"],
         data: { 
             id: 'Acetaminophen',
-            classes:["drug"],
             chembl_code: 'CHEMBL112'
         }
     },
     {
         group: "nodes",
+        classes: ["adverse event"],
         data: {
             id: 'Acute hepatic failure',
-            classes: ["adverse event"],
             meddraCode: '10000804'
         }
     },
     {
         group: "nodes",
+        classes: ["adverse event"],
         data: {
             id: 'Toxicity to various agents',
-            classes: ["adverse event"],
             meddraCode: '10070863'
         }
+    },
+    {
+        group: "nodes",
+        classes: ["proteinTarget"],
+        data: {
+            id: 'Vanilloid receptor',
+            parent: "pathway_xyz",
+        }
+    },
+    {
+        group: "nodes",
+        classes: ["proteinTarget"],
+        data: {
+            id: 'XYZ receptor',
+            parent: "pathway_xyz",
+        }
+    },
+    {
+        group: "nodes",
+        classes: ["proteinTarget"],
+        data: {
+            id: 'Cyclooxygenase',  
+        },
+        
     },
     { 
         group: "edges",
@@ -35,6 +66,38 @@ let initialElements = [
             target: 'Acute hepatic failure', 
             arrow: "vee",
             critval: 123 
+        }
+    },
+    {
+        group: "edges",
+        data: {
+            id: 'edge_2',
+            source: 'Acetaminophen',
+            target: 'Toxicity to various agents',
+            arrow: "vee",
+            critval: 123
+        }
+    },
+    {
+        group: "edges",
+        classes: ["drug_target"],
+        data: {
+            id: 'edge_3',
+            source: 'Acetaminophen',
+            target: 'Vanilloid receptor',
+            arrow: "vee",
+            action: "inhibits",
+        }
+    },
+    {
+        group: "edges",
+        classes: ["drug_target"],
+        data: {
+            id: 'edge_4',
+            source: 'Acetaminophen',
+            target: 'Cyclooxygenase',
+            arrow: "vee",
+            action: "opener",
         }
     },
 ]
@@ -51,9 +114,24 @@ function setupCy(){
                 selector: 'node',
                 style: {
                     'background-color': '#666',
-                    'label': 'data(id)'
+                    'label': 'data(id)',
                 }
             },
+            {
+                selector: '.pathway',
+                style: {
+                    "background-color": "red",
+                    "opacity": "0.3"
+                }
+            },
+            {
+                selector: '.proteinTarget',
+                style: {
+                    "background-color": "blue",
+                    "color": "blue",
+                    "opacity": "1"
+                }
+            },   
 
             {
                 selector: 'edge',
@@ -63,18 +141,26 @@ function setupCy(){
                     'target-arrow-color': '#ccc',
                     'target-arrow-shape': 'triangle',
                     'curve-style': 'bezier',
-                    "label": "causes"
+                    "label": "causes",
                 }
-            }
+            },
+            
+            {
+                selector: '.drug_target',
+                style: {
+                    "label": "data(action)",
+                }
+            },
+            
         ],
 
         layout: {
-            name: 'grid',
-            rows: 2
+            name: 'circle',
         }
 
     });
 
+    // enable pop-ups on tap
     cy.on("tap","node", function(hoverEvent) {
         let node = hoverEvent.target;
         let popup = node.popper({
@@ -90,12 +176,11 @@ function setupCy(){
             popper: {} // my popper options here
         });
 
+        // have the pop-up's position update when the node is moved
         let update = () => {
             popup.update();
         };
-
         node.on('position', update);
-
         cy.on('pan zoom resize', update);
     })
 
