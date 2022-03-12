f445c0f3ec5e081aa39a0edf1780f405feb4f597
Aleksander Aleksiev <aleksaleksiev@outlook.com>
2022-03-13 10:43:17 +1100

    Minor edit to force CI re-run

--- springdb/src/test/java/com/semis/gradvek/springdb/SpringdbApplicationTests.java
+++ springdb/src/test/java/com/semis/gradvek/springdb/SpringdbApplicationTests.java
@@ -3,6 +3,8 @@ package com.semis.gradvek.springdb;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class SpringdbApplicationTests {
 
