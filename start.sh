418f53cb90669938ca70cff836dc9344a2f73aa0
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2022-03-20 22:37:18 -0400

    Docker compose for cloud deployment

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+trap "kill -2 0" EXIT
+
 # Start the back end
+
+# https://stackoverflow.com/a/2013589
+NEO4JHOST="${NEO4JHOST:-$(for i in $(echo "$(cat /proc/net/route | head -2 | tail -1 | awk '{print $3}')" | sed -E 's/(..)(..)(..)(..)/\4 \3 \2 \1/' ) ; do printf "%d." $((16#$i)); done | sed 's/.$//')}"
+
 # https://unix.stackexchange.com/a/561524
-export NEO4JURL=bolt://$(for i in $(echo "$(cat /proc/net/route | head -2 | tail -1 | awk '{print $3}')" | sed -E 's/(..)(..)(..)(..)/\4 \3 \2 \1/' ) ; do printf "%d." $((16#$i)); done | sed 's/.$//'):7687
+export NEO4JURL=bolt://$NEO4JHOST:7687
+
 java -jar /app.jar &
 
 # Start the front end
