218c2d347c9cb7e9c1435cb361d0a8ddde1a9fd6
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2022-03-21 09:18:32 -0400

    Install script runnable from cloud instance

---
+++ demo/install.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/capstone-SEMIS/gradvek/docker-compose/demo/docker-compose.yml > docker-compose.yml
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose pull
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose up --detach --remove-orphans
+docker image prune -f
