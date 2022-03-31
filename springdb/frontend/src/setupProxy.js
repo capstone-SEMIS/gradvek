eca4c4adff0ba6ebd0f2f07a90b2d997249de36a
datailluminations <datailluminations@gmail.com>
2022-03-31 09:38:21 -0700

    fixed fetchAPI code

--- springdb/frontend/src/setupProxy.js
+++ springdb/frontend/src/setupProxy.js
@@ -7,12 +7,3 @@ module.exports = function(app) {
   });
   app.use("/api", backend);
 };
-
-const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-};
-
-app.use(cors(corsOptions)); // Use this after the variable declaration
