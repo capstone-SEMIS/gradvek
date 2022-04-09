d93d8cb49a9476b010099fd9288c8de8a9f3f9d9
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2022-04-09 14:59:50 -0400

    Add CSV help text

--- springdb/frontend/src/sections/@dashboard/datamgmt/CsvImporter.js
+++ springdb/frontend/src/sections/@dashboard/datamgmt/CsvImporter.js
@@ -2,23 +2,25 @@ import {styled} from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import {useState} from "react";
-import {Box, CircularProgress, ClickAwayListener} from "@mui/material";
+import {Box, CircularProgress} from "@mui/material";
 import CancelIcon from '@mui/icons-material/Cancel';
 import CheckIcon from '@mui/icons-material/Check';
 import HelpIcon from '@mui/icons-material/Help';
+
+const CsvBox = styled(Box)({
+    fontFamily: 'monospace', whiteSpace: 'pre-line', marginLeft: '2em'
+});
 
 const Input = styled('input')({
     display: 'none',
 });
 
 const CenteredSpan = styled('span')({
-    display: 'flex',
-    alignItems: 'center'
+    display: 'flex', alignItems: 'center'
 })
 
 const SpacedSpan = styled('span')({
-    marginLeft: '1em',
-    marginRight: '1em'
+    marginLeft: '1em', marginRight: '1em'
 })
 
 function IconicMessage(props) {
@@ -32,25 +34,37 @@ function Helper() {
         setOpen((prev) => !prev);
     };
 
-    const handleClickAway = () => {
-        setOpen(false);
-    };
+    const samples = [
+        ['Node: AdverseEvent', 'Import the adverse event somnolence with MedDRA code 10041349', 'node,meddraCode,adverseEventId', 'AdverseEvent,10041349,Somnolence'],
+        ['Node: Drug', 'Import the drug acetaminophen with ChEMBL code CHEMBL112', 'node,chembl_code,drugId', 'Drug,CHEMBL112,Acetaminophen'],
+        ['Node: Target', 'Import the target vanilloid receptor with symbol TRPV1 and Ensembl code ENSG00000196689', 'node,targetId,symbol,name', 'Target,ENSG00000196689,TRPV1,Vanilloid receptor'],
+        ['Node: Pathway', 'Import the pathway TRP channels with Reactome identifier R-HSA-3295583 and top-level parent transport of small molecules', 'node,pathwayId,pathwayCode,topLevelTerm', 'Pathway,R-HSA-3295583,TRP channels,Transport of small molecules'],
+        ['Relationship: Associated With', 'Import the relationship that Acetaminophen is associated with somnolence with a log likelihood ratio of 1023.49 and a critical value of 374.79', 'relationship,chembl_code,meddraCode,llr,critval', 'ASSOCIATED_WITH,CHEMBL112,10041349,1023.49,374.79'],
+        ['Relationship: Targets', 'Import the relationship that acetaminophen targets TRPV1 as an opener', 'relationship,chembl_code,targetId,actionType', 'TARGETS,CHEMBL112,ENSG00000196689,OPENER'],
+        ['Relationship: Participates', 'Import the relationship that target TRPV1 participates in the TRP channels pathway', 'relationship,targetId,pathwayId', 'PARTICIPATES_IN,ENSG00000196689,R-HSA-3295583']
+    ];
 
-    return (
-        <ClickAwayListener onClickAway={handleClickAway}>
-            <Box sx={{position: 'relative'}}>
-                <Button onClick={handleClick}>
-                    <HelpIcon color='info'/>
-                    <SpacedSpan> CSV file format </SpacedSpan>
-                </Button>
-                {open ? (
-                    <Box>
-                        Here's how to format the CSV!
-                    </Box>
-                ) : null}
-            </Box>
-        </ClickAwayListener>
-    );
+    return (<Box sx={{position: 'relative'}}>
+            <Button onClick={handleClick}>
+                <HelpIcon color='info'/>
+                <SpacedSpan>
+                    {open ? 'Hide help' : 'CSV file format'}
+                </SpacedSpan>
+            </Button>
+            {open ? (<Box margin='1em'>
+                <div>Here are examples of CSV files you can import.</div>
+                {samples.map(s => {
+                    return (<div key={s[0]} style={{marginTop: '1.5em'}}>
+                            <div style={{marginBottom: '0.5em'}}>
+                                <span style={{fontWeight: 'bold'}}>{s[0]}</span>
+                                <span style={{marginLeft: '1.5em'}}>{s[1]}</span>
+                            </div>
+                            <CsvBox>{s[2]}</CsvBox>
+                            <CsvBox>{s[3]}</CsvBox>
+                        </div>);
+                })}
+            </Box>) : null}
+        </Box>);
 }
 
 export default function Uploader() {
@@ -65,20 +79,16 @@ export default function Uploader() {
         formData.append('baseUrl', baseUrl);
         setProgress(SPINNING);
         fetch('/api/csv', {
-            method: 'post',
-            body: formData
-        }).then(response => response.json().then(
-            body => ({
-                ok: response.ok,
-                body: body
-            }))
-        ).then(result => {
-            if (result.ok) {
+            method: 'post', body: formData
+        }).then(response => {
+            if (response.ok) {
                 setProgress(SUCCESS);
             } else {
-                setProgress(FAILURE);
+                throw new Error(response.statusText);
             }
-        })
+        }).catch(() => {
+            setProgress(FAILURE);
+        });
     }
 
     function getProgressIndicator() {
@@ -94,16 +104,14 @@ export default function Uploader() {
         }
     }
 
-    return (
-        <Stack direction="column" spacing={2}>
-            <Stack direction="row" alignItems="center" spacing={2}>
-                <label htmlFor="contained-button-file">
-                    <Input accept=".csv" id="contained-button-file" type="file" onChange={onFileChangeHandler}/>
-                    <Button variant="contained" component="span">Upload</Button>
-                </label>
-                {getProgressIndicator()}
-            </Stack>
-            <Helper/>
+    return (<Stack direction="column" spacing={2}>
+        <Stack direction="row" alignItems="center" spacing={2}>
+            <label htmlFor="contained-button-file">
+                <Input accept=".csv" id="contained-button-file" type="file" onChange={onFileChangeHandler}/>
+                <Button variant="contained" component="span">Upload</Button>
+            </label>
+            {getProgressIndicator()}
         </Stack>
-    );
+        <Helper/>
+    </Stack>);
 }
