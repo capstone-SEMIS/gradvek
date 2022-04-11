3ebf9000d923847a894a4ff0eadcf64c2939070d
datailluminations <datailluminations@gmail.com>
2022-04-11 12:10:23 -0700

    fix

--- springdb/frontend/src/App.js
+++ springdb/frontend/src/App.js
@@ -1,10 +1,11 @@
+import React from "react";
 // routes
-import Router from './routes';
+import Router from "./routes";
 // theme
-import ThemeConfig from './theme';
-import GlobalStyles from './theme/globalStyles';
+import ThemeConfig from "./theme";
+import GlobalStyles from "./theme/globalStyles";
 // components
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from "./components/ScrollToTop";
 
 // ----------------------------------------------------------------------
 
