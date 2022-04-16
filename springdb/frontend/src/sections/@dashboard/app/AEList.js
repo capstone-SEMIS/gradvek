750df40db850fbc8921b11702619be11f6ab3ba0
datailluminations <datailluminations@gmail.com>
2022-04-16 12:19:09 -0700

    fix build

--- springdb/frontend/src/sections/@dashboard/app/AEList.js
+++ springdb/frontend/src/sections/@dashboard/app/AEList.js
@@ -10,18 +10,16 @@ AEList.propTypes = {
   graphNodes: PropTypes.array.isRequired
 };
 
-
 export default function AEList({ graphNodes, filterHandler }) {
   let AEs = graphNodes.filter(graphNode => {
     return graphNode.classes?.includes("adverse event");
   });
   console.log("AEs", AEs);
 
-  listOfDrugs = {};
+  // listOfDrugs = {};
   return (
     <Card>
       <Accordion title="Title" drugs="Drugs" weight={9812734} />
     </Card>
   );
-
 }
