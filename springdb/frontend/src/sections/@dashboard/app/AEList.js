e75323b4d6850f2e22b6d369177361d59034a9fe
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2022-04-16 17:01:55 -0400

    Fix inadvertent commit

--- springdb/frontend/src/sections/@dashboard/app/AEList.js
+++ springdb/frontend/src/sections/@dashboard/app/AEList.js
@@ -1,25 +1,45 @@
 import PropTypes from "prop-types";
 // material
-
-import { Card } from "@mui/material";
-import Accordion from "./Accordion.js";
+import {Card, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
 
 // ----------------------------------------------------------------------
 
 AEList.propTypes = {
-  graphNodes: PropTypes.array.isRequired
+    graphNodes: PropTypes.array.isRequired
 };
 
-export default function AEList({ graphNodes, filterHandler }) {
-  let AEs = graphNodes.filter(graphNode => {
-    return graphNode.classes?.includes("adverse event");
-  });
-  console.log("AEs", AEs);
-
-  // listOfDrugs = {};
-  return (
-    <Card>
-      <Accordion title="Title" drugs="Drugs" weight={9812734} />
-    </Card>
-  );
+export default function AEList({graphNodes, filterHandler}) {
+    let AEs = graphNodes.filter((graphNode) => {
+        return graphNode.classes?.includes("adverse-event");
+    });
+    return (
+        <Card>
+            <TableContainer>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>
+                                Adverse Event
+                            </TableCell>
+                            <TableCell>
+                                Weight
+                            </TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {AEs.sort((a, b) => b.data.llr - a.data.llr).map((AE) => (
+                            <TableRow key={AE.data.id} onClick={(e) => filterHandler(AE.data.id)}>
+                                <TableCell>
+                                    {AE.data.name}
+                                </TableCell>
+                                <TableCell>
+                                    {AE.data.llr}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Card>
+    )
 }
