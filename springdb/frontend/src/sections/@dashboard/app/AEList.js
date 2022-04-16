cc68ab6ae2ed3988d8d271dcd648f064c10898f4
datailluminations <datailluminations@gmail.com>
2022-04-16 12:46:23 -0700

    changes to Accordion and AEList

--- springdb/frontend/src/sections/@dashboard/app/AEList.js
+++ springdb/frontend/src/sections/@dashboard/app/AEList.js
@@ -16,10 +16,52 @@ export default function AEList({ graphNodes, filterHandler }) {
   });
   console.log("AEs", AEs);
 
-  // listOfDrugs = {};
+  let listOfDrugs = [
+    {
+      title: "Tylenol",
+      list: [
+        {
+          drug: "test",
+          weight: 3435
+        },
+        {
+          drug: "test2",
+          weight: 45435
+        },
+        {
+          drug: "test3",
+          weight: 9835
+        }
+      ]
+    },
+    {
+      title: "Prozac",
+      list: [
+        {
+          drug: "Proztest",
+          weight: 43435
+        },
+        {
+          drug: "Proztest2",
+          weight: 5435
+        },
+        {
+          drug: "Proztest3",
+          weight: 79935
+        }
+      ]
+    }
+  ];
   return (
     <Card>
-      <Accordion title="Title" drugs="Drugs" weight={9812734} />
+      {listOfDrugs.map(item => (
+        <Accordion
+          title="Tylenol"
+          drugs="Drugs"
+          weight={9812734}
+          list={item.list}
+        />
+      ))}
     </Card>
   );
 }
--- springdb/frontend/src/sections/@dashboard/app/CytoCanvas.js
+++ springdb/frontend/src/sections/@dashboard/app/CytoCanvas.js
@@ -1,133 +1,141 @@
-import cytoscape from 'cytoscape';
-import avsdf from 'cytoscape-avsdf';
+import cytoscape from "cytoscape";
+import avsdf from "cytoscape-avsdf";
 
-import {Component} from 'react';
+import { Component } from "react";
 
 // ----------------------------------------------------------------------
-cytoscape.use( avsdf );
+cytoscape.use(avsdf);
 export default class CytoCanvas extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div
+        id="cyto_canvas"
+        style={this.canvasStyle}
+        ref={el => (this.el = el)}
+      />
+    );
+  }
+
+  componentDidMount() {
+    // on initial mount, create a new cytoscape canvas
+    // and hook that canvas up to this element's div
+    let cytoInstance = cytoscape({
+      container: this.el,
+      style: this.initialStyles
+    });
+
+    this.setState({
+      cytoInstance: cytoInstance
+    });
+  }
+
+  componentWillUnmount() {
+    // when this React component is unmounted, also unmount the Cytoscape canvas
+    this.state.cytoInstance.unmount();
+    this.state.cytoInstance.destroy();
+  }
+
+  componentDidUpdate() {
+    // replace all elements with the current graphNodes
+    this.state.cytoInstance.remove("*");
+    this.state.cytoInstance.add(this.props.graphNodes);
+
+    if ("AE" in this.props.focusNode) {
+      let nodeToFocus = this.state.cytoInstance.elements(
+        `node#${this.props.focusNode.AE}`
+      );
+      let neighbouringNodes = nodeToFocus.closedNeighborhood();
+
+      // show neighbouring elements only
+      neighbouringNodes.style("display", "element");
+      neighbouringNodes.layout({ name: "breadthfirst" }).run();
+      // neighbouringNodes.layout({name:"circle"}).run();
+      this.state.cytoInstance.fit(neighbouringNodes);
+    } else {
+      // no focusNode, so show all nodes
+      this.state.cytoInstance
+        .elements(this.props.nodeFilter)
+        .style("display", "element");
+      // lay all elements out in a circle
+      this.state.cytoInstance.layout({ name: "avsdf" }).run();
     }
-    
-    render(){
-        return (
-                <div id="cyto_canvas" style={this.canvasStyle} ref={el => this.el = el} /> 
-        );
+  }
+
+  initialStyles = [
+    // the stylesheet for the graph
+    {
+      selector: "node",
+      style: {
+        "background-color": "#8b786d",
+        color: "#8b786d",
+        label: "data(name)",
+        "text-valign": "center",
+        "text-outline-color": "white",
+        // 'text-outline-opacity': '50%',
+        "text-outline-width": "2px"
+        // 'text-background-padding' : '2px',
+        // 'text-border-width': '1px',
+        // 'text-border-color': 'white',
+      }
+    },
+    {
+      selector: ".pathway",
+      style: {
+        "background-color": "#78a1bb",
+        "background-opacity": "0.15"
+      }
+    },
+    {
+      selector: ".target",
+      style: {
+        "background-color": "#78a1bb",
+        color: "#78a1bb",
+        opacity: "1"
+      }
+    },
+    {
+      selector: ".drug",
+      style: {
+        "background-color": "lightcoral",
+        color: "lightcoral",
+        opacity: "1"
+      }
+    },
+
+    {
+      selector: "edge",
+      style: {
+        width: 3,
+        "line-color": "#ccc",
+        "target-arrow-color": "#ccc",
+        "target-arrow-shape": "triangle",
+        "curve-style": "bezier",
+        label: "data(action)",
+        "text-rotation": "autorotate"
+      }
+    },
+
+    // {
+    //     selector: '.drug_target',
+    //     style: {
+    //         "label": "data(action)",
+    //     }
+    // },
+    {
+      selector: "*",
+      style: {
+        display: "none"
+      }
     }
+  ];
 
-    componentDidMount() {
-        // on initial mount, create a new cytoscape canvas
-        // and hook that canvas up to this element's div
-        let cytoInstance = cytoscape({
-            container: this.el,
-            style: this.initialStyles,
-        });
-
-        this.setState({
-            cytoInstance: cytoInstance
-        })
-    }
-
-    componentWillUnmount(){
-        // when this React component is unmounted, also unmount the Cytoscape canvas
-        this.state.cytoInstance.unmount();
-        this.state.cytoInstance.destroy();
-    }
-
-    componentDidUpdate() {
-        // replace all elements with the current graphNodes
-        this.state.cytoInstance.remove("*");
-        this.state.cytoInstance.add(this.props.graphNodes); 
-
-        if ('AE' in this.props.focusNode) {
-            let nodeToFocus = this.state.cytoInstance.elements(`node#${this.props.focusNode.AE}`)
-            let neighbouringNodes = nodeToFocus.closedNeighborhood();
-
-            // show neighbouring elements only
-            neighbouringNodes.style("display", "element");
-            neighbouringNodes.layout({name:"breadthfirst"}).run();
-            // neighbouringNodes.layout({name:"circle"}).run();
-            this.state.cytoInstance.fit(neighbouringNodes);
-        }
-        else {
-            // no focusNode, so show all nodes
-            this.state.cytoInstance.elements(this.props.nodeFilter).style("display", "element");
-            // lay all elements out in a circle
-            this.state.cytoInstance.layout({ name: "avsdf" }).run();
-        }
-    }
- 
-    initialStyles = [ // the stylesheet for the graph
-        {
-            selector: 'node',
-            style: {
-                'background-color': '#8b786d',
-                "color": "#8b786d",
-                'label': 'data(name)',
-                'text-valign': 'center',
-                'text-outline-color': 'white',
-                // 'text-outline-opacity': '50%',
-                'text-outline-width' : '2px',
-                // 'text-background-padding' : '2px',
-                // 'text-border-width': '1px',
-                // 'text-border-color': 'white',
-            }
-        },
-        {
-            selector: '.pathway',
-            style: {
-                "background-color": "#78a1bb",
-                "background-opacity": "0.15"
-            }
-        },
-        {
-            selector: '.target',
-            style: {
-                "background-color": "#78a1bb",
-                "color": "#78a1bb",
-                "opacity": "1"
-            }
-        },
-        {
-            selector: '.drug',
-            style: {
-                "background-color": "lightcoral",
-                "color": "lightcoral",
-                "opacity": "1"
-            }
-        },
-
-        {
-            selector: 'edge',
-            style: {
-                'width': 3,
-                'line-color': '#ccc',
-                'target-arrow-color': '#ccc',
-                'target-arrow-shape': 'triangle',
-                'curve-style': 'bezier',
-                "label": "data(action)",
-                "text-rotation": "autorotate",
-            }
-        },
-
-        // {
-        //     selector: '.drug_target',
-        //     style: {
-        //         "label": "data(action)",
-        //     }
-        // },
-        {
-            selector: '*',
-            style: {
-                "display": "none"
-            }
-        }
-    ]
-
-    canvasStyle = {
-        width: '100%',
-        minHeight: '300px'
-    }
+  canvasStyle = {
+    width: "100%",
+    minHeight: "300px"
+  };
 }
