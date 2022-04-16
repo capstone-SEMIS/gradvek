8fd7ebcfb75d1f85ec9af1dc8f707547d40c4325
datailluminations <datailluminations@gmail.com>
2022-04-16 12:14:55 -0700

    fix build

--- springdb/frontend/src/sections/@dashboard/app/AEList.js
+++ springdb/frontend/src/sections/@dashboard/app/AEList.js
@@ -1,15 +1,7 @@
 import PropTypes from "prop-types";
 // material
 
-import {
-  Card,
-  TableRow,
-  TableCell,
-  TableHead,
-  TableContainer,
-  Table,
-  TableBody
-} from "@mui/material";
+import { Card } from "@mui/material";
 import Accordion from "./Accordion.js";
 
 // ----------------------------------------------------------------------
@@ -22,7 +14,9 @@ export default function AEList({ graphNodes, filterHandler }) {
   let AEs = graphNodes.filter(graphNode => {
     return graphNode.classes?.includes("adverse event");
   });
+  console.log("AEs", AEs);
 
+  listOfDrugs = {};
   return (
     <Card>
       <Accordion title="Title" drugs="Drugs" weight={9812734} />
