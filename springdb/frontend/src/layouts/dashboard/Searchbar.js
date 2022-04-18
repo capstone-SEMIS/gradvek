826f78a2c9bc663c540def966b9d7321099ba36d
Michael Haak <31259830+Michael9094@users.noreply.github.com>
2022-04-18 13:13:51 -0400

    Switch from side bar to top bar

--- springdb/frontend/src/layouts/dashboard/Searchbar.js
+++ springdb/frontend/src/layouts/dashboard/Searchbar.js
@@ -21,7 +21,8 @@ const RootStyle = styled(AppBar)(({ theme }) => ({
     backgroundColor: alpha(theme.palette.background.default, 0.72),
     [theme.breakpoints.up('lg')]: {
         width: `calc(100% - ${DRAWER_WIDTH + 1}px)`
-    }
+    },
+    position: "sticky"
 }));
 
 const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
