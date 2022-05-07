bf8cbbb2edd7ff4d0af93f52a2e08fa4fe10e5ce
datailluminations <datailluminations@gmail.com>
2022-05-06 20:15:10 -0700

    add new topnavbar background

--- springdb/frontend/src/layouts/dashboard/DashboardTopbar.js
+++ springdb/frontend/src/layouts/dashboard/DashboardTopbar.js
@@ -1,21 +1,21 @@
 import {Box, IconButton, Toolbar} from "@mui/material";
 import Logo from "../../components/Logo";
-import {lightBlue} from "@mui/material/colors";
+// import {lightBlue} from "@mui/material/colors";
 import Iconify from "../../components/Iconify";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import "./DashboardTopbar.css";
 
 export default function DashboardTopbar() {
     return (
-        <Toolbar sx={{backgroundColor: lightBlue["100"], width: "100%", alignItems: 'center', flexWrap: 'wrap'}}>
+        <Toolbar  sx={{width: "100%", alignItems: 'center', flexWrap: 'wrap'}} className="dashboard-topbar-background" >
             <IconButton edge="start">
                 <Logo/>
             </IconButton>
             <Typography variant="h6" component="div" sx={{mr: 10}}>
                 GRADVEK
             </Typography>
-
-            <Box>
+                       <Box >
                 <Button color='inherit' href='/dashboard/app'>
                     <Iconify icon='ant-design:search' width={22} height={22} sx={{mr: 1}}/>
                     <Typography variant="subtitle1" component="div" sx={{mr: 10}}>
